c71a665717c4084b25cd17571491a18137b878d7
Abraham <abukip344@gmail.com>
2024-07-06 08:46:24 -0400

    phase-4 code challenge

--- server/models.py
+++ server/models.py
@@ -21,8 +21,17 @@ class Restaurant(db.Model, SerializerMixin):
     address = db.Column(db.String)
 
     # add relationship
+    restaurant_pizzas = db.relationship('RestaurantPizza', back_populates='restaurant', cascade='delete')
 
     # add serialization rules
+    serialize_rules = ('-restaurant_pizzas.restaurant',)
+
+    # def to_dict(self):
+    #     return {
+    #         'id' : self.id,
+    #         'name' : self.name,
+    #         'address' : self.address
+    #     }
 
     def __repr__(self):
         return f"<Restaurant {self.name}>"
@@ -36,8 +45,10 @@ class Pizza(db.Model, SerializerMixin):
     ingredients = db.Column(db.String)
 
     # add relationship
+    restaurant_pizzas = db.relationship('RestaurantPizza', back_populates='pizza', cascade='delete')
 
     # add serialization rules
+    serialize_rules = ('-restaurant_pizzas.pizza',)
 
     def __repr__(self):
         return f"<Pizza {self.name}, {self.ingredients}>"
@@ -48,12 +59,24 @@ class RestaurantPizza(db.Model, SerializerMixin):
 
     id = db.Column(db.Integer, primary_key=True)
     price = db.Column(db.Integer, nullable=False)
+    pizza_id = db.Column(db.Integer, db.ForeignKey('pizzas.id'))
+    restaurant_id = db.Column(db.Integer, db.ForeignKey('restaurants.id'))
 
     # add relationships
+    restaurant = db.relationship('Restaurant', back_populates='restaurant_pizzas')
+    pizza = db.relationship('Pizza', back_populates='restaurant_pizzas')
 
     # add serialization rules
-
+    serialize_rules = ('-restaurant.restaurant_pizzas', '-pizza.restaurant_pizzas')
     # add validation
+    @validates('price')
+    def validate_price(self,key, the_price):
+        if 1 <= the_price <= 30:
+            return the_price
+        else:
+            raise ValueError('Price must be between 1 and 30')
+            
+
 
     def __repr__(self):
         return f"<RestaurantPizza ${self.price}>"
